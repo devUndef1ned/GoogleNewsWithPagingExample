caaf31273a3221cccb6d4d5a19cd0f1cd7f6a635
Oleg Kuzmin <o.kuzmin@dodopizza.com>
2019-09-16 22:19:47 +0300

    Added test and change behaviour according to tests

--- app/src/main/java/com/devundefined/googlenewswithpagingexample/infrastructure/ArticleLoaderImpl.kt
+++ app/src/main/java/com/devundefined/googlenewswithpagingexample/infrastructure/ArticleLoaderImpl.kt
@@ -1,30 +1,41 @@
 package com.devundefined.googlenewswithpagingexample.infrastructure
 
+import android.annotation.SuppressLint
 import com.devundefined.googlenewswithpagingexample.domain.Article
 import com.devundefined.googlenewswithpagingexample.domain.loader.ArticleLoader
 import com.devundefined.googlenewswithpagingexample.domain.loader.LoadResult
 import com.devundefined.googlenewswithpagingexample.infrastructure.backend.ArticleDto
 import com.devundefined.googlenewswithpagingexample.infrastructure.backend.NewsApi
 import kotlinx.coroutines.*
-import java.util.*
+import java.text.SimpleDateFormat
 
 class ArticleLoaderImpl(private val newsApi: NewsApi, private val apiKey: String, private val country: String) : ArticleLoader {
+
+    companion object {
+        @SuppressLint("SimpleDateFormat")
+        private val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    }
 
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     private val toModel: (ArticleDto) -> Article = { dto ->
-        with(dto) { Article(source.name, author, title, description, url, imageUrl, Date()) }
+        with(dto) { Article(source.name, author, title, description, url, imageUrl, sdf.parse(date)) }
     }
 
     override fun loadArticles(countPerPage: Int, pageNumber: Int): LoadResult {
         return runBlocking {
             withContext(scope.coroutineContext) {
-                val result = newsApi.getNews(apiKey, country, countPerPage, pageNumber)
-                if (result.status != "ok") {
-                    LoadResult.Error(IllegalStateException(result.message))
-                } else {
-                    LoadResult.Data(result.articles.map(toModel), result.totalResult!!)
+                try {
+                    val result = newsApi.getNews(apiKey, country, countPerPage, pageNumber)
+                    if (result.status != "ok") {
+                        LoadResult.Error(IllegalStateException(result.message))
+                    } else {
+                        LoadResult.Data(result.articles.map(toModel), result.totalResult!!)
+                    }
+                } catch (e: Exception) {
+                    LoadResult.Error(e)
                 }
+
             }
         }
     }
---
+++ app/src/test/java/com/devundefined/googlenewswithpagingexample/infrastructure/ArticleLoaderTest.kt
@@ -0,0 +1,107 @@
+package com.devundefined.googlenewswithpagingexample.infrastructure
+
+import com.devundefined.googlenewswithpagingexample.domain.Article
+import com.devundefined.googlenewswithpagingexample.domain.loader.LoadResult
+import com.devundefined.googlenewswithpagingexample.infrastructure.backend.ArticleDto
+import com.devundefined.googlenewswithpagingexample.infrastructure.backend.NewsApi
+import com.devundefined.googlenewswithpagingexample.infrastructure.backend.NewsDto
+import com.devundefined.googlenewswithpagingexample.infrastructure.backend.SourceDto
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class ArticleLoaderTest {
+
+    @Mock
+    lateinit var newsApi: NewsApi
+
+    private val loader by lazy { ArticleLoaderImpl(newsApi, "apiKey", "country") }
+
+    @Test
+    fun shouldReturnArticles_whenLoadSuccessfullyFromApi() {
+        runBlocking {
+            val dto1 = ArticleDto(
+                SourceDto("sourceId1", "source1"),
+                "author1",
+                "title1",
+                "description1",
+                "url1",
+                "imageUrl1",
+                "2019-09-13T18:27:00Z"
+            )
+            val dto2 = ArticleDto(
+                SourceDto("sourceId2", "source2"),
+                "author2",
+                "title2",
+                "description2",
+                "url2",
+                "imageUrl2",
+                "2019-09-13T18:14:04Z"
+            )
+            whenever(newsApi.getNews(any(), any(), any(), any())).thenReturn(
+                NewsDto(
+                    status = "ok",
+                    code = null,
+                    message = null,
+                    totalResult = 2,
+                    articles = listOf(dto1, dto2)
+                )
+            )
+
+            val result = loader.loadArticles(countPerPage = 3, pageNumber = 1)
+
+            assertTrue(result is LoadResult.Data)
+            (result as LoadResult.Data).articles.also { loadedArticles ->
+                assertTrue(loadedArticles.any { it.matches(dto1) })
+                assertTrue(loadedArticles.any { it.matches(dto2) })
+            }
+        }
+    }
+
+    @Test
+    fun shouldReturnErrorResult_whenLoadThroughApiFailed() {
+        runBlocking {
+            whenever(
+                newsApi.getNews(
+                    any(),
+                    any(),
+                    any(),
+                    any()
+                )
+            ).thenThrow(IllegalArgumentException("Failed to load any data"))
+
+            val result = loader.loadArticles(countPerPage = 10, pageNumber = 1)
+
+            assertTrue(result is LoadResult.Error)
+            assertTrue((result as LoadResult.Error).cause is IllegalArgumentException)
+        }
+    }
+
+    @Test
+    fun shouldReturnErrorResult_whenApiReturnedResultWithError() {
+        runBlocking {
+            whenever(newsApi.getNews(any(), any(), any(), any())).thenReturn(
+                NewsDto(
+                    status = "error", code = "Something is wrong", message =
+                    "Something is wrong error message", totalResult = null, articles = listOf()
+                )
+            )
+
+            val result = loader.loadArticles(countPerPage = 10, pageNumber = 1)
+
+            assertTrue(result is LoadResult.Error)
+            assertTrue((result as LoadResult.Error).cause is IllegalStateException)
+        }
+    }
+
+    private fun Article.matches(dto: ArticleDto): Boolean {
+        return sourceName == dto.source.name && author == dto.author && title == dto.title
+                && description == dto.description && url == dto.url && imageUrl == dto.imageUrl
+    }
+}
